7c69eb9bf9cd8a59d1e607c05690c58e46fb91a2
Xin Zhi <115723006+xong002@users.noreply.github.com>
2023-04-20 15:58:03 +0800

    workshop complete

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,9 +1,45 @@
 package sg.edu.nus.iss;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.*;
+import java.net.Socket;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        Integer port = Integer.parseInt(args[0]);
+
+        Socket socket = new Socket("localhost", port);
+
+        Console con = System.console();
+        String keyboardInput = "";
+        String msgReceived = "";
+
+        try (InputStream is = socket.getInputStream()) {
+            BufferedInputStream bis = new BufferedInputStream(is);
+            DataInputStream dis = new DataInputStream(bis);
+
+            try (OutputStream os = socket.getOutputStream()) {
+                BufferedOutputStream bos = new BufferedOutputStream(os);
+                DataOutputStream dos = new DataOutputStream(bos);
+                
+                while(!keyboardInput.equals("close")){
+                    keyboardInput = con.readLine("Enter a command: ");
+
+                    dos.writeUTF(keyboardInput);
+                    dos.flush();
+
+                    msgReceived = dis.readUTF();
+                    System.out.println(msgReceived);
+                }
+
+                dos.close();
+                bos.close();
+            } catch (EOFException e) {}
+            
+            dis.close();
+            bis.close();
+            socket.close();
+
+        } catch (EOFException e) {}
+
     }
 }
